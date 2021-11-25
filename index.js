9c6863844ad378efa5039a4e4e947ac11791f4df
Mufidat3250 <wahabmufidat1919@gmail.com>
2021-11-25 14:13:34 +0100

    update exercise3.5

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const { request, response } = require("express");
 const express = require("express");
+const morgan = require("morgan");
 
 const app = express();
+app.use(express.json());
+app.use(morgan("tiny"));
 
 let persons = [
   {
@@ -52,7 +55,28 @@ app.get(`/api/persons/:id`, (request, response) => {
     response.status(404).send("<h1>Error 404</h1>");
   }
 });
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((person, index) => person.id !== id);
+  request.status(204).end();
+});
 
+app.post("/api/persons/", (request, response) => {
+  let body = request.body;
+  console.log(body);
+  if (!body.name && !body.number) {
+    return response.status(400).json({ error: "missing name" });
+  }
+
+  const person_ = {
+    id: Math.floor(Math.random() * 1000),
+    name: body.name,
+    number: body.number,
+  };
+
+  persons = persons.concat(person_);
+  response.json(person_);
+});
 const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port${PORT}`);
