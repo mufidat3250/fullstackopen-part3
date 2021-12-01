df23d60933d324d765a917b87708e3f3d757f76a
Mufidat3250 <wahabmufidat1919@gmail.com>
2021-12-01 19:15:09 +0100

    update p

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ app.post("/api/persons/", (request, response) => {
   persons = persons.concat(person_);
   response.json(person_);
 });
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port${PORT}`);
 });
